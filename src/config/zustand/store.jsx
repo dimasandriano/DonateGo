89bb4fd53f828c836084a4f47ddc8fd295c48d42
dimasandriano <duangdeong@gmail.com>
2023-05-07 15:51:13 +0700

    fix slice

--- src/config/zustand/store.jsx
+++ src/config/zustand/store.jsx
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { loginSlice } from "./LoginSlice";
+import { loginSlice } from "./loginSlice";
 
 export const store = create(
 	persist(
