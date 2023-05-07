b097c734054764bb1a3dea28d1e1f6cc49a63b45
dimasandriano <duangdeong@gmail.com>
2023-05-07 17:59:15 +0700

    fix navbar

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../component/Organisms/Navbar/Navbar";
+import Navbar from "../component/organisms/Navbar/Navbar";
 import Footer from "../component/organisms/Footer/Footer";
 
 function RootLayout({ children }) {
